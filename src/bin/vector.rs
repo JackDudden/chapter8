d438a4920158e0ab48a0b0b5609c715eff9f223c
Jack Dudden <jack@dudden.uk>
2022-11-12 18:35:16 +0000

    feat: vector mode valculation

--- src/bin/vector.rs
+++ src/bin/vector.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello World!");
+
+    let mut numbers = String::new();
+    io::stdin()
+        .read_line(&mut numbers)
+        .ok()
+        .expect("Numbers could not be read");
+    let numbers: Vec<i32> = numbers
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let mean = mean_fnc(&numbers);
+    println!("Mean is {}", mean);
+}
+
+fn mean_fnc(vect: &Vec<i32>) -> f64 {
+    let mut total = 0;
+    for value in vect {
+        total += value
+    };
+    let total = total as f64;
+    total / vect.len() as f64
 }
